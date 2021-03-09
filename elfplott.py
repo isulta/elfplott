585f9cdd0576df1340028b9c957c5f0d926ce9f5
Imran Sultan <isultan030@gmail.com>
2021-03-09 01:50:49 -0600

    Fix E field plotting; add B field plotting on yz-planes

--- elfplott.py
+++ elfplott.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-np.seterr(divide='ignore')
+# np.seterr(divide='ignore')
 
 class RestFrame:
-    coords1d = np.linspace(-5,5,20)
-    X, Y, Z = np.meshgrid(coords1d, coords1d, np.linspace(-5,5,21))
-    z0 = 10 # index of z-dimension coressponding to z=0 (i.e. xy-plane)
+    coords1d = np.linspace(-5,5,20)                         # x and y coordinates
+    coords1d_z = np.linspace(-5,5,21)                       # z coordinates
+    X, Y, Z = np.meshgrid(coords1d, coords1d, coords1d_z)
+    z0 = list(coords1d_z).index(0.)                         # index of z-dimension corresponding to z=0 (i.e. xy-plane)
     
     def __init__(self):
         self.Ex = np.zeros_like(RestFrame.X)
@@ -30,24 +31,39 @@ class RestFrame:
 
     def plot_E_xyplane(self, plot_prime=False, show_charges=True):
         plt.figure(dpi=150)
-        plt.streamplot(self.X[:,:,RestFrame.z0], self.Y[:,:,RestFrame.z0], self.Ex[:,:,RestFrame.z0], self.Ey[:,:,RestFrame.z0], density=2, linewidth=.5)
+        plt.streamplot(RestFrame.coords1d, RestFrame.coords1d, self.Ex[:,:,RestFrame.z0], self.Ey[:,:,RestFrame.z0], density=2, linewidth=.5)
         
         if plot_prime:
-            plt.streamplot(self.X[:,:,RestFrame.z0], self.Y[:,:,RestFrame.z0], self.Ex_prime[:,:,RestFrame.z0], self.Ey_prime[:,:,RestFrame.z0], density=2, linewidth=.5)
+            plt.streamplot(RestFrame.coords1d, RestFrame.coords1d, self.Ex_prime[:,:,RestFrame.z0], self.Ey_prime[:,:,RestFrame.z0], density=2, linewidth=.5)
 
         if show_charges:
             for xq, yq, q in self.point_charges:
                 if (np.abs(xq)<=RestFrame.coords1d[-1]) and (np.abs(yq)<=RestFrame.coords1d[-1]):
                     plt.scatter(xq, yq, marker=('_' if q<0 else '+'), c=('k' if q<0 else 'r'), zorder=4, s=100)
         
+        plt.xlabel("$x$")
+        plt.ylabel("$y$")
+        plt.title(r"$\vec{E}$ projection on $xy$-plane")
+
+        plt.show()
+
+    def plot_B(self, x_idx):
+        plt.figure(dpi=150)
+
+        plt.streamplot(RestFrame.coords1d_z, RestFrame.coords1d, self.Bz_prime[:,x_idx,:], self.By_prime[:,x_idx,:], density=2, linewidth=.5)
+
+        plt.xlabel("$z$")
+        plt.ylabel("$y$")
+        plt.title(r"$\vec{B'}$ projection on $yz$-plane, " + f"$x={RestFrame.coords1d[x_idx]}$")
+        
         plt.show()
 
     def add_point_charge(self, xq, yq, q):
-        X1 = self.X - xq
-        Y1 = self.Y - yq
+        X1 = RestFrame.X - xq
+        Y1 = RestFrame.Y - yq
         Phi1 = np.arctan2(Y1, X1)
-        Theta1 = np.arctan( np.sqrt(X1**2 + Y1**2)/self.Z )
-        E = q / (X1**2 + Y1**2 + self.Z**2)
+        Theta1 = np.arctan2( np.sqrt(X1**2 + Y1**2), RestFrame.Z )
+        E = q / (X1**2 + Y1**2 + RestFrame.Z**2)
         self.Ex += E * np.sin(Theta1) * np.cos(Phi1)
         self.Ey += E * np.sin(Theta1) * np.sin(Phi1)
         self.Ez += E * np.cos(Theta1)
